cf577115cfd91f71fd6bf861e6fb8b22242cf04b
Haris Hamzić <53975836+hamzic2019@users.noreply.github.com>
2020-09-03 23:06:36 +0200

    Styling class update Updated multiple files with class name, for styling purposes.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer(){
   const year = new Date().getFullYear();
   return(
-    <div>
+    <div className="footer">
       <p>All Rights Reserved - © {year} Haris Hamzic</p>
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Note(){
   return(
-    <div>
+    <div className="task">
       <h1>This is title</h1>
       <p>This is task which has to be done</p>
     </div>
