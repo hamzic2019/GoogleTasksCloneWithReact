151904d8c73d6ca851d9e508432d231884fbca61
Haris Hamzić <53975836+hamzic2019@users.noreply.github.com>
2020-09-03 22:44:03 +0200

    Initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function App() {
+  return(
+    <div> 
+      <h1>HI THER</h1>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const rootElement = document.getElementById("root");
+import App from './components/app'
+
+
+ReactDOM.render(<App />, rootElement);
