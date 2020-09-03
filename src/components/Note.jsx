796411794417b9a297e0ae82fa552fd81b2fe25c
Haris Hamzić <53975836+hamzic2019@users.noreply.github.com>
2020-09-03 23:01:44 +0200

    Created Note.jsx

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function Note(){
+  return(
+    <div>
+      <h1>This is title</h1>
+      <p>This is task which has to be done</p>
+    </div>
+  );
+}
